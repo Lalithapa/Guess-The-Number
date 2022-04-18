37984cd46d56224babdc3558c7058aed35bb6d5a
Lalit Thapa <98501775+Lalithapa@users.noreply.github.com>
2022-04-19 05:01:52 +0530

    update script.js

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
-
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   if (guess === 0) {
-    document.querySelector('.message').textContent = '🔴No Number';
-  } else if (guess === secretNumber) {
+    displayMessage('🔴No Number');
+  } //when the player wins
+  else if (guess === secretNumber) {
     //when guess is right
-    document.querySelector('.message').textContent = 'Correct Number🥳';
+    displayMessage('Correct Number🥳');
     // score++;
     // document.querySelector('.score').textContent = score;
     document.querySelector('body').style.backgroundColor = 'lightgreen';
@@ -20,51 +23,50 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  }
-  //When Guess is Wrong
-  else if (guess > secretNumber && guess <= 20) {
+  } else if (guess !== secretNumber && guess <= 20) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High📈';
+      // document.querySelector('.message').textContent =guess > secretNumber ? 'Too High📈' : 'Too Low📉';
+      displayMessage(guess > secretNumber ? 'Too High📈' : 'Too Low📉');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You Lose';
+      displayMessage('You Lose');
       document.querySelector('.score').textContent = 0;
     }
   }
+  //When Guess is high
+  // else if (guess > secretNumber && guess <= 20) {
+  //   if (score > 1) {
+  //     displayMessage('Too High📈');
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     displayMessage('You Lose';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
   //When Guess is Low
-  else if (guess < secretNumber && guess <= 20) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low📉';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You Lose';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else {
-    document.querySelector('.message').textContent =
-      'Type Number Between 1 to 20';
+  // else if (guess < secretNumber && guess <= 20) {
+  //   if (score > 1) {
+  //     displayMessage('Too Low📉');
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     displayMessage('You Lose';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  //}
+  else {
+    displayMessage('Type Number Between 1 to 20');
   }
 });
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = ' ';
+  document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-
-/*Coding Challenge #1
-Implement a game rest functionality, so that the player can make a new guess!
-Your tasks:
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the 'score' and
-'secretNumber' variables
-3. Restore the initial conditions of the message, number, score and guess input
-fields
-4. Also restore the original background color (#222) and number width (15rem)
-GOOD LUCK �*/
